8c01ba0d0638c5606605a5161f92e336901454a7
lorenzocazzador <cazzadorlorenzo.1@gmail.com>
2024-12-07 00:13:43 +0100

    modified sample to generate a huge number of samples

--- src/sample.py
+++ src/sample.py
@@ -60,7 +60,9 @@ def parse_checkpoint_filename(filename, default_params=None):
 # Argument parser
 parser = ArgumentParser()
 parser.add_argument("checkpoint", type=str, help="Path to the checkpoint file")
+parser.add_argument("--num_samples", type=int, default=100, help="Number of samples to generate")
 parser.add_argument("--skip_fid", action="store_true", help="Skip the computation of the FID score")
+parser.add_argument("--skip_plot", action="store_true", help="Skip the plotting of the generated samples")
 
 args = parser.parse_args()
 
@@ -101,46 +103,24 @@ ddpm_light = DdpmLight.load_from_checkpoint(args.checkpoint, ddpmnet=model)
 ddpm_light.eval().to(device)
 
 # Generate samples
-sample_size = 100
-columnrow = int(math.sqrt(sample_size))
+sample_size = args.num_samples
+batch_size = 100
+columnrow = int(math.sqrt(batch_size))
 
 # Generate class labels for conditional sampling
 klass = T.cat([T.full((columnrow,), i) for i in range(columnrow)])
 klass = klass.to(device)
 
-logger.info(f"Generating {sample_size} samples...")
-with T.no_grad():
-    generated_samples = ddpm_light.sample(sample_size, klass).view(sample_size, num_channels, 32, 32).to(device)
+# Initialize FID metric
+fid = FrechetInceptionDistance(feature=2048, normalize=True, reset_real_features=False, input_img_size=(3,32,32)).to(device)
+fid.set_dtype(T.float64)
 
-# Normalize generated samples to [0, 1]
-generated_samples = ((generated_samples + 1) / 2).clamp(0, 1)
-if num_channels == 1:
-    generated_samples = generated_samples.repeat(1, 3, 1, 1)  # Convert to RGB 
-
-# Visualize generated samples
-logger.info("Saving generated samples...")
-fig = plt.figure(figsize=(8, 8))
-columns = columnrow
-rows = columnrow
-for i in range(1, columns * rows + 1):
-    img = generated_samples[i - 1].cpu().detach().numpy().transpose(1, 2, 0).squeeze()
-    fig.add_subplot(rows, columns, i)
-    plt.axis('off')
-    if num_channels == 1:
-        plt.imshow(img, cmap="gray")
-    else:
-        plt.imshow(img)
-
-if args.skip_fid is not True:
-    generated_samples = generated_samples.to(dtype=T.float64)
-    # Initialize FID metric
-    fid = FrechetInceptionDistance(feature=2048, normalize=True).to(device)
-
-    # Add real images to FID
-    logger.info("Adding real images to FID computation...")
-    _, _, test_dataset = get_dataset(checkpoint_params['dataset_name'])
-    test_dataloader = DataLoader(test_dataset, batch_size=checkpoint_params['bs'], shuffle=False)
-    for real_batch in test_dataloader:
+# Add real images to FID
+logger.info("Adding real images to FID computation...")
+if args.skip_fid is False:
+    train_dataset, _, _ = get_dataset(checkpoint_params['dataset_name'])
+    train_dataloader = DataLoader(train_dataset, batch_size=checkpoint_params['bs'], shuffle=False)
+    for real_batch in train_dataloader:
         real_images, _ = real_batch
         # Normalize real images to [0, 1]
         real_images = ((real_images + 1) / 2).clamp(0, 1).to(device)
@@ -149,18 +129,44 @@ if args.skip_fid is not True:
         # Move real images to CPU
         real_images = real_images.to(dtype=T.float64)
         fid.update(real_images, real=True)
-        break  # Use only the first batch for simplicity
 
-    # Add generated images to FID
-    logger.info("Adding generated images to FID computation...")
-    fid.update(generated_samples, real=False)
+with T.no_grad():
+    for i in range(sample_size // batch_size):
+        logger.info(f"Generating samples {i * batch_size} to {(i + 1) * batch_size}")
+        generated_samples = ddpm_light.sample(batch_size, klass).view(batch_size, num_channels, 32, 32).to(device)
 
-    # Compute FID score
-    fid_score = fid.compute()
-    logger.info(f"FID Score: {fid_score}")
+        # Normalize generated samples to [0, 1]
+        generated_samples = ((generated_samples + 1) / 2).clamp(0, 1)
+        if num_channels == 1:
+            generated_samples = generated_samples.repeat(1, 3, 1, 1)  # Convert to RGB 
 
-filename = f"samples/{checkpoint_params['dataset_name']}_{checkpoint_params['cond']}_{checkpoint_params['unet_dim']}_{checkpoint_params['beta_schedule']}_{checkpoint_params['epoch']}_{checkpoint_params['val_loss']}"
-if args.skip_fid is not True:
-    filename += f"_fid={fid_score:.2f}"
-filename += ".png"
-plt.savefig(filename)
+        if args.skip_fid is False:
+            # Add generated images to FID
+            logger.info("Adding generated images to FID computation...")
+            generated_samples = generated_samples.to(dtype=T.float64)
+            fid.update(generated_samples, real=False)
+
+# Compute FID score
+fid_score = fid.compute()
+logger.info(f"FID Score: {fid_score}")     
+
+# Visualize generated samples
+if args.skip_plot is False:
+    logger.info("Saving generated samples...")
+    fig = plt.figure(figsize=(8, 8))
+    columns = columnrow
+    rows = columnrow
+    for i in range(1, columns * rows + 1):
+        img = generated_samples[i - 1].cpu().detach().numpy().transpose(1, 2, 0).squeeze()
+        fig.add_subplot(rows, columns, i)
+        plt.axis('off')
+        if num_channels == 1:
+            plt.imshow(img, cmap="gray")
+        else:
+            plt.imshow(img)
+
+    filename = f"samples/{checkpoint_params['dataset_name']}_{checkpoint_params['cond']}_{checkpoint_params['unet_dim']}_{checkpoint_params['beta_schedule']}_{checkpoint_params['epoch']}_{checkpoint_params['val_loss']}"
+    if args.skip_fid is False:
+        filename += f"_fid={fid_score:.2f}"
+    filename += ".png"
+    plt.savefig(filename)
